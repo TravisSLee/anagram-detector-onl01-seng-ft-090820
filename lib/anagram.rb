6aff4354f601fb70e3ed6f6676cb69a700360ff2
Travis Lee <travisslee223@gmail.com>
2020-09-21 18:43:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,8 +4,8 @@ class Anagram
   attr_accessor :word
   
   def match(array)
-  array.select do |word|
-    word.split("").sort == @word.split("").sort
-  end
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
 end
